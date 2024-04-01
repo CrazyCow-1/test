2c22a596a76fefe57e5b4fb031044f03aeed3e9a
CrazyCow-1 <154741545+CrazyCow-1@users.noreply.github.com>
2024-04-01 15:41:35 +0900

    Update TTD.js

--- TTD.js
+++ TTD.js
@@ -1,22 +1,22 @@
-let inputBox = document.getElementById('inputField');  // 할 일 입력창
-let addToDo = document.getElementById('addToDo');      // 버튼
-let toDoList = document.getElementById('toDoList');    // 할 일 리스트창
+let inputBox = document.getElementById('inputField');  
+let addToDo = document.getElementById('addToDo');      
+let toDoList = document.getElementById('toDoList');    
 
-addToDo.addEventListener('click', function(){    // 버튼에 클릭 이벤트가 발생하면
-    var list = document.createElement('li');     // html 'li' 태그 만들기
-    if (!inputBox.value)            // 할 일 입력창에 내용이 입력되지 않으면 alert 발생
+addToDo.addEventListener('click', function(){    
+    var list = document.createElement('li');     
+    if (!inputBox.value)            
         alert('내용을 입력해 주세요!');
     else
     {
-        list.innerText = inputBox.value;  // <li>입력된 할 일</li>
-        toDoList.appendChild(list);       // 할 일 리스트창에 자식으로 붙이기
-        inputBox.value= "";               // 할 일 입력창 초기화
+        list.innerText = inputBox.value;  
+        toDoList.appendChild(list);       
+        inputBox.value= "";               
     }
 
-    list.addEventListener('click', function(){      // 만들어진 list에 클릭 이벤트가 발생하면 줄 긋기
+    list.addEventListener('click', function(){      
         list.style.textDecoration = "line-through";
     })
-    list.addEventListener('dblclick', function(){   // list에 더블클릭 이벤트가 발생하면 할 일 리스트창에서 지우기
+    list.addEventListener('dblclick', function(){   
         toDoList.removeChild(list);
     })
 })
